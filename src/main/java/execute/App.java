b699905c2a051ed0940ded262c0680f1f51c1247
prebou <prebou1999@gmail.com>
2024-11-15 23:11:43 +0100

    tp noté update

--- src/main/java/execute/App.java
+++ src/main/java/execute/App.java
@@ -11,23 +11,27 @@ import jakarta.persistence.Persistence;
 import java.util.List;
 
 public class App {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("petstore");
-    EntityManager em = emf.createEntityManager();
-    em.getTransaction().begin();
-    // Créer un nouveau magasin
-    PetStore petStore = new PetStore("Animalerie","Pqul");
-    // ... remplir les données du magasin
-    Address address = new Address("17","rue stalingrad","92700","Colombes");
-    // ... remplir les données de l'adresse
-    petStore.setAddress(address);
+    public static void main(String[] args) {
 
 
-    em.persist(petStore);
-    em.getTransaction().commit();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        // Créer un nouveau magasin
+        PetStore petStore = new PetStore("Animalerie", "Paul");
+        // ... remplir les données du magasin
+        Address address = new Address("17", "rue stalingrad", "92700", "Colombes");
+        // ... remplir les données de l'adresse
+        petStore.setAddress(address);
 
-    // Récupérer tous les animaux d'un magasin
-    List<Animal> animals = em.createQuery("SELECT a FROM Animal a WHERE a.petStore = :petStore", Animal.class).setParameter("petStore", petStore).getResultList();
 
-    em.close();
-    emf.close();
+        em.persist(petStore);
+        em.getTransaction().commit();
+
+        // Récupérer tous les animaux d'un magasin
+        List<Animal> animals = em.createQuery("SELECT a FROM Animal a WHERE a.petStore = :petStore", Animal.class).setParameter("petStore", petStore).getResultList();
+
+        em.close();
+        emf.close();
+    }
 }
